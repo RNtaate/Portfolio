954cea066e1fdc1126b310955f789713a50a1a51
RNtaate <royhyde14@gmail.com>
2020-11-11 21:41:48 +0300

    Add event listeners necessary for viewing model overlays

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,30 @@ function openMenu() {
 function closeMenu() {
   document.getElementById('mobile-menu').style.width = '0%';
 }
+
+function closeModel() {
+  elements = document.getElementsByClassName('card-wrapper-div');
+  for(var i = 0; i < elements.length; i ++){
+    elements[i].style.visibility = 'hidden';
+    elements[i].style.opacity = '0';
+    elements[i].style.transform = 'scale(0)';
+
+    elements[i].style.top = '-100%';
+  }
+}
+
+window.onload = function() {
+  list = document.getElementsByClassName('see-btn');
+  for(var i = 0; i < list.length; i ++){
+    list[i].addEventListener('click', function(e){
+      var att = this.getAttribute('id')
+      let card = document.getElementById('card' + att.toString());
+
+      card.style.visibility = 'visible';
+      card.style.opacity = '1';
+      card.style.top = '0';
+      card.style.transform = 'scale(1)';
+      e.preventDefault();
+    });
+  }
+}
